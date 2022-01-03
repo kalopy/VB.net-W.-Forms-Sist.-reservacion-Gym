85f26b537db9a64b16ae0002a672d1274a425f15
kalopy <carlosozuna49@gmail.com>
2022-01-03 18:28:12 -0300

    Agregar archivos de proyecto.

---
+++ Gym/SRA.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SRA
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cmbLocal = New System.Windows.Forms.ComboBox()
+        Me.cmbModalidad = New System.Windows.Forms.ComboBox()
+        Me.lblLocal = New System.Windows.Forms.Label()
+        Me.lblModalidad = New System.Windows.Forms.Label()
+        Me.txtDocumento = New System.Windows.Forms.TextBox()
+        Me.lblDocumento = New System.Windows.Forms.Label()
+        Me.cmbHorario = New System.Windows.Forms.ComboBox()
+        Me.lblHorario = New System.Windows.Forms.Label()
+        Me.btnVerificarCupo = New System.Windows.Forms.Button()
+        Me.lblNombreEstado = New System.Windows.Forms.Label()
+        Me.btnConfirmar = New System.Windows.Forms.Button()
+        Me.dtFechaAsistencia = New System.Windows.Forms.DateTimePicker()
+        Me.lblCupos = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAlumnoEstado = New System.Windows.Forms.Label()
+        Me.lblDisponible = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmbLocal
+        '
+        Me.cmbLocal.FormattingEnabled = True
+        Me.cmbLocal.Location = New System.Drawing.Point(208, 98)
+        Me.cmbLocal.Name = "cmbLocal"
+        Me.cmbLocal.Size = New System.Drawing.Size(153, 21)
+        Me.cmbLocal.TabIndex = 0
+        '
+        'cmbModalidad
+        '
+        Me.cmbModalidad.FormattingEnabled = True
+        Me.cmbModalidad.Location = New System.Drawing.Point(208, 149)
+        Me.cmbModalidad.Name = "cmbModalidad"
+        Me.cmbModalidad.Size = New System.Drawing.Size(153, 21)
+        Me.cmbModalidad.TabIndex = 1
+        '
+        'lblLocal
+        '
+        Me.lblLocal.AutoSize = True
+        Me.lblLocal.Location = New System.Drawing.Point(100, 106)
+        Me.lblLocal.Name = "lblLocal"
+        Me.lblLocal.Size = New System.Drawing.Size(33, 13)
+        Me.lblLocal.TabIndex = 2
+        Me.lblLocal.Text = "Local"
+        '
+        'lblModalidad
+        '
+        Me.lblModalidad.AutoSize = True
+        Me.lblModalidad.Location = New System.Drawing.Point(100, 152)
+        Me.lblModalidad.Name = "lblModalidad"
+        Me.lblModalidad.Size = New System.Drawing.Size(56, 13)
+        Me.lblModalidad.TabIndex = 3
+        Me.lblModalidad.Text = "Modalidad"
+        '
+        'txtDocumento
+        '
+        Me.txtDocumento.Location = New System.Drawing.Point(208, 12)
+        Me.txtDocumento.Name = "txtDocumento"
+        Me.txtDocumento.Size = New System.Drawing.Size(200, 20)
+        Me.txtDocumento.TabIndex = 4
+        '
+        'lblDocumento
+        '
+        Me.lblDocumento.AutoSize = True
+        Me.lblDocumento.Location = New System.Drawing.Point(100, 15)
+        Me.lblDocumento.Name = "lblDocumento"
+        Me.lblDocumento.Size = New System.Drawing.Size(85, 13)
+        Me.lblDocumento.TabIndex = 5
+        Me.lblDocumento.Text = "Nro. Documento"
+        '
+        'cmbHorario
+        '
+        Me.cmbHorario.FormattingEnabled = True
+        Me.cmbHorario.Location = New System.Drawing.Point(208, 202)
+        Me.cmbHorario.Name = "cmbHorario"
+        Me.cmbHorario.Size = New System.Drawing.Size(153, 21)
+        Me.cmbHorario.TabIndex = 9
+        '
+        'lblHorario
+        '
+        Me.lblHorario.AutoSize = True
+        Me.lblHorario.Location = New System.Drawing.Point(100, 210)
+        Me.lblHorario.Name = "lblHorario"
+        Me.lblHorario.Size = New System.Drawing.Size(41, 13)
+        Me.lblHorario.TabIndex = 10
+        Me.lblHorario.Text = "Horario"
+        '
+        'btnVerificarCupo
+        '
+        Me.btnVerificarCupo.Location = New System.Drawing.Point(208, 243)
+        Me.btnVerificarCupo.Name = "btnVerificarCupo"
+        Me.btnVerificarCupo.Size = New System.Drawing.Size(87, 30)
+        Me.btnVerificarCupo.TabIndex = 11
+        Me.btnVerificarCupo.Text = "Verificar Cupo"
+        Me.btnVerificarCupo.UseVisualStyleBackColor = True
+        '
+        'lblNombreEstado
+        '
+        Me.lblNombreEstado.AutoSize = True
+        Me.lblNombreEstado.Location = New System.Drawing.Point(205, 45)
+        Me.lblNombreEstado.Name = "lblNombreEstado"
+        Me.lblNombreEstado.Size = New System.Drawing.Size(0, 13)
+        Me.lblNombreEstado.TabIndex = 13
+        '
+        'btnConfirmar
+        '
+        Me.btnConfirmar.Location = New System.Drawing.Point(238, 317)
+        Me.btnConfirmar.Name = "btnConfirmar"
+        Me.btnConfirmar.Size = New System.Drawing.Size(89, 34)
+        Me.btnConfirmar.TabIndex = 14
+        Me.btnConfirmar.Text = "Confirmar"
+        Me.btnConfirmar.UseVisualStyleBackColor = True
+        '
+        'dtFechaAsistencia
+        '
+        Me.dtFechaAsistencia.Location = New System.Drawing.Point(208, 60)
+        Me.dtFechaAsistencia.Name = "dtFechaAsistencia"
+        Me.dtFechaAsistencia.Size = New System.Drawing.Size(200, 20)
+        Me.dtFechaAsistencia.TabIndex = 15
+        '
+        'lblCupos
+        '
+        Me.lblCupos.AutoSize = True
+        Me.lblCupos.Location = New System.Drawing.Point(244, 301)
+        Me.lblCupos.Name = "lblCupos"
+        Me.lblCupos.Size = New System.Drawing.Size(0, 13)
+        Me.lblCupos.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(100, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Fecha de asistencia"
+        '
+        'lblAlumnoEstado
+        '
+        Me.lblAlumnoEstado.AutoSize = True
+        Me.lblAlumnoEstado.Location = New System.Drawing.Point(208, 41)
+        Me.lblAlumnoEstado.Name = "lblAlumnoEstado"
+        Me.lblAlumnoEstado.Size = New System.Drawing.Size(77, 13)
+        Me.lblAlumnoEstado.TabIndex = 18
+        Me.lblAlumnoEstado.Text = "Nombre&Estado"
+        '
+        'lblDisponible
+        '
+        Me.lblDisponible.AutoSize = True
+        Me.lblDisponible.Location = New System.Drawing.Point(322, 252)
+        Me.lblDisponible.Name = "lblDisponible"
+        Me.lblDisponible.Size = New System.Drawing.Size(56, 13)
+        Me.lblDisponible.TabIndex = 19
+        Me.lblDisponible.Text = "Disponible"
+        '
+        'SRA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 409)
+        Me.Controls.Add(Me.lblDisponible)
+        Me.Controls.Add(Me.lblAlumnoEstado)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblCupos)
+        Me.Controls.Add(Me.dtFechaAsistencia)
+        Me.Controls.Add(Me.btnConfirmar)
+        Me.Controls.Add(Me.lblNombreEstado)
+        Me.Controls.Add(Me.btnVerificarCupo)
+        Me.Controls.Add(Me.lblHorario)
+        Me.Controls.Add(Me.cmbHorario)
+        Me.Controls.Add(Me.lblDocumento)
+        Me.Controls.Add(Me.txtDocumento)
+        Me.Controls.Add(Me.lblModalidad)
+        Me.Controls.Add(Me.lblLocal)
+        Me.Controls.Add(Me.cmbModalidad)
+        Me.Controls.Add(Me.cmbLocal)
+        Me.Name = "SRA"
+        Me.Text = "Sistema de Reserva de Asistencia"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbLocal As ComboBox
+    Friend WithEvents cmbModalidad As ComboBox
+    Friend WithEvents lblLocal As Label
+    Friend WithEvents lblModalidad As Label
+    Friend WithEvents txtDocumento As TextBox
+    Friend WithEvents lblDocumento As Label
+    Friend WithEvents cmbHorario As ComboBox
+    Friend WithEvents lblHorario As Label
+    Friend WithEvents txtprueba As TextBox
+    Friend WithEvents btnVerificarCupo As Button
+    Friend WithEvents lblNombreEstado As Label
+    Friend WithEvents btnConfirmar As Button
+    Friend WithEvents dtFechaAsistencia As DateTimePicker
+    Friend WithEvents lblCupos As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblAlumnoEstado As Label
+    Friend WithEvents lblDisponible As Label
+End Class
---
+++ Gym/SRA.vb
@@ -0,0 +1,366 @@
+﻿Imports System.Data.SqlClient
+
+Public Class SRA
+    Dim miConexion As New SqlConnection("data source = KALO; initial catalog = Gym; user id = kalo; password = kalo")
+    Dim miComandoSQL As SqlCommand
+    Dim instruccionSQL As String
+    Dim dataReader As SqlDataReader
+
+    Private Sub SRA_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        CargarCmbLocal()
+        CargarCmbModalidad()
+
+    End Sub
+
+    Sub CargarCmbLocal()
+        instruccionSQL = "select * from local"
+        miConexion.Open()
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+        miConexion.Close()
+        cmbLocal.DataSource = tabla
+        cmbLocal.DisplayMember = "Nombre"
+        cmbLocal.ValueMember = "LocalID"
+    End Sub
+
+    Sub CargarCmbModalidad()
+        instruccionSQL = "select * from Modalidad"
+        miConexion.Open()
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+        miConexion.Close()
+        cmbModalidad.DataSource = tabla
+        cmbModalidad.DisplayMember = "Descripcion"
+        cmbModalidad.ValueMember = "ModalidadID"
+    End Sub
+
+    'FUNCION RECUPERAR DATOS DEL ALUMNO
+    Function RecuperarDatosV2(nroDocumento As Integer) As String
+
+        If ValidarExisteAlumno(txtDocumento.Text) = True And ValidarEstadoAlumno(txtDocumento.Text) = True Then
+
+            miConexion.Open()
+            instruccionSQL = "select * from Alumno where NroDocumento = '" & nroDocumento & "'"
+            miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+            Dim adaptador As New SqlDataAdapter(miComandoSQL)
+            miConexion.Close()
+            Dim tabla As New DataTable
+            adaptador.Fill(tabla)
+
+            If tabla.Rows.Count > 0 Then
+                Return tabla.Rows(0)(2).ToString() & ", " & "Estado: Al dia"
+            Else
+                MsgBox("No existe")
+            End If
+
+        Else
+
+            miConexion.Open()
+            instruccionSQL = "select * from Alumno where NroDocumento = '" & nroDocumento & "'"
+            miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+            Dim adaptador As New SqlDataAdapter(miComandoSQL)
+            miConexion.Close()
+            Dim tabla As New DataTable
+            adaptador.Fill(tabla)
+
+            If tabla.Rows.Count > 0 Then
+                Return tabla.Rows(0)(2).ToString() & ", " & "Estado: Moroso"
+            Else
+            MsgBox("No existe")
+        End If
+
+        End If
+
+    End Function
+
+    'FUNCION PARA BUSCAR Y VALIDAR SI EL ALUMNO EXISTE
+    Function ValidarExisteAlumno(ByVal nroDocumento As Integer) As Boolean
+        Dim resultado As Boolean = False
+
+        miConexion.Open()
+        instruccionSQL = "Select * from Alumno where NroDocumento = '" & nroDocumento & "'"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+
+        dataReader = miComandoSQL.ExecuteReader
+
+        If dataReader.Read Then
+            resultado = True
+        End If
+
+        dataReader.Close()
+        miConexion.Close()
+
+        Return resultado
+    End Function
+
+    'FUNCION PARA RECUPERAR FECHA_VENCIMIENTO Y VALIDAR EL ESTADO(Al dia/Moroso) DEL ALUMNO
+    Function ValidarEstadoAlumno(ByVal nroDocumento As Integer) As Boolean
+
+        Dim resultado As Boolean = False
+
+        miConexion.Open()
+        instruccionSQL = "select i.FechaVencimiento 
+                          from Alumno a join Inscripcion i on a.AlumnoID = i.AlumnoID 
+                          where a.NroDocumento = '" & nroDocumento & "' and i.FechaVencimiento > GETDATE()"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+
+        dataReader = miComandoSQL.ExecuteReader
+
+        If dataReader.Read Then
+            resultado = True
+        End If
+
+        dataReader.Close()
+        miConexion.Close()
+
+        Return resultado
+    End Function
+
+    'Evento “Leave” del control que contiene el número de documento............................
+    Private Sub txtDocumento_Leave(sender As Object, e As EventArgs) Handles txtDocumento.Leave
+        'Validando que no ingrese el campo vacio, y que lo que ingrese sea solo nros.
+        If IsNumeric(txtDocumento.Text) And txtDocumento.Text.Equals("") = False Then
+            Dim documento As Integer
+            documento = Val(txtDocumento.Text)
+            lblAlumnoEstado.Text = RecuperarDatosV2(documento)
+        Else
+            MsgBox("No puede dejar campos vacios y debe ingresar solo numeros")
+        End If
+
+    End Sub
+
+    'EVENTO SelectedIndexChanged
+    Private Sub cmbModalidad_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbModalidad.SelectedIndexChanged
+
+        Dim cod As Integer = Val(cmbModalidad.SelectedValue.ToString)
+
+        instruccionSQL = "select HorarioID, Descripcion from Horario where ModalidadID =" & cod
+        miConexion.Open()
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+        miConexion.Close()
+        cmbHorario.DataSource = tabla
+        cmbHorario.DisplayMember = "Descripcion"
+        cmbHorario.ValueMember = "HorarioID"
+
+    End Sub
+
+    Private Sub cmbModalidad_SelectedValueChanged(sender As Object, e As EventArgs) Handles cmbModalidad.SelectedValueChanged
+
+        Dim cod As Integer = Val(cmbModalidad.SelectedValue.ToString)
+
+        instruccionSQL = "select HorarioID, Descripcion from Horario where ModalidadID =" & cod
+        miConexion.Open()
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+        miConexion.Close()
+        cmbHorario.DataSource = tabla
+        cmbHorario.DisplayMember = "Descripcion"
+        cmbHorario.ValueMember = "HorarioID"
+
+    End Sub
+
+    'VALIDAR QUE HAYA SELECCIONADO UN HORARIO............
+    Private Function ValidarSeleccionHorario() As Boolean
+        If (cmbHorario.SelectedIndex.Equals(-1)) Then
+
+            MessageBox.Show("Debe seleccionar un horario")
+
+            Return False
+
+        End If
+
+        Return True
+
+    End Function
+
+    'VERIFICAR CUPO
+    Private Sub btnVerificarCupo_Click(sender As Object, e As EventArgs) Handles btnVerificarCupo.Click
+
+        If ValidarSeleccionHorario() = True Then
+
+            'Profe, mi instruccionSQL en este Tema parace un caos, pero funciona...
+            miConexion.Open()
+            instruccionSQL = "If exists (select (h.Limite - COUNT(ReservaID)) as Disponible 
+                              from Horario h join Reserva r on h.HorarioID = r.HorarioID
+						                     join Modalidad m on m.ModalidadID = r.ModalidadID
+                              where h.Descripcion = '" + cmbHorario.Text + "'  and m.Descripcion = '" + cmbModalidad.Text + "'
+                              group by h.Limite)
+
+                              select (h.Limite - COUNT(ReservaID)) as Disponible 
+                              from Horario h join Reserva r on h.HorarioID = r.HorarioID
+						                     join Modalidad m on m.ModalidadID = r.ModalidadID
+                              where h.Descripcion = '" + cmbHorario.Text + "' and m.Descripcion = '" + cmbModalidad.Text + "'
+                              group by h.Limite
+
+                              Else (select h.Limite from Horario h join Modalidad m 
+						      on h.ModalidadID = m.ModalidadID
+                              where m.Descripcion = '" + cmbModalidad.Text + "'
+						      group by h.Limite)"
+
+            miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+            Dim adaptador As New SqlDataAdapter(miComandoSQL)
+            Dim tabla As New DataTable
+            adaptador.Fill(tabla)
+            miConexion.Close()
+
+            lblDisponible.Text = tabla.Rows(0)(0).ToString()
+
+            If lblDisponible.Text < 1 Then
+                MsgBox("Ya no hay cupos disponibles")
+            End If
+
+        End If
+
+    End Sub
+
+    'VALIDAR QUE HAYA SELECCIONADO UN LOCAL............
+    Private Function ValidarSeleccionLocal() As Boolean
+        If (cmbLocal.SelectedIndex.Equals(-1)) Then
+
+            MessageBox.Show("Debe seleccionar un local")
+
+            Return False
+
+        End If
+
+        Return True
+
+    End Function
+
+    'VALIDAR QUE HAYA SELECCIONADO UNA MODALIDAD...........
+    Private Function ValidarSeleccionModalidad() As Boolean
+        If (cmbModalidad.SelectedIndex.Equals(-1)) Then
+
+            MessageBox.Show("Debe seleccionar una modalidad")
+
+            Return False
+
+        End If
+
+        Return True
+
+    End Function
+
+    'FUNCION PARA RECUPERAR LocalID FILTRANDO MEDIANTE EL LOCAL SELECCIONADO EN EL COMBOBOX cmbLocal
+    Function RecuperarLocalID() As Integer
+
+        miConexion.Open()
+        instruccionSQL = "select LocalID from Local where Nombre = '" + cmbLocal.Text + "'"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        miConexion.Close()
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+        Return tabla.Rows(0)(0)
+
+    End Function
+
+    'FUNCION PARA RECUPERAR ModalidadID FILTRANDO MEDIANTE LA MODALIDAD SELECCIONADO EN EL COMBOBOX cmbModalidad
+    Function RecuperarModalidadlID() As Integer
+
+        miConexion.Open()
+        instruccionSQL = "select ModalidadID from Modalidad where Descripcion = '" + cmbModalidad.Text + "'"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        miConexion.Close()
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+        Return tabla.Rows(0)(0).ToString()
+
+    End Function
+
+    'FUNCION PARA RECUPERAR HorarioID FILTRANDO MEDIANTE LA MODALIDAD Y EL HORARIO SELECCIONADO
+    Function RecuperarHorarioID() As Integer
+
+        miConexion.Open()
+        instruccionSQL = "select h.HorarioID FROM Horario h join Modalidad m on h.ModalidadID = m.ModalidadID
+                          where m.Descripcion = '" + cmbModalidad.Text + "' and h.Descripcion  = '" + cmbHorario.Text + "'"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        miConexion.Close()
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+        miConexion.Close()
+
+        Return tabla.Rows(0)(0).ToString()
+
+    End Function
+
+    'FUNCION PARA RECUPERAR AlumnoID FILTRANDO MEDIANTE EL NRO DE DCOUMENTO INGRESADO EN EL txtDocumento
+    Function RecuperarAlumnolID() As Integer
+
+        miConexion.Open()
+        instruccionSQL = "select AlumnoID from Alumno where NroDocumento = '" + txtDocumento.Text + "'"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        miConexion.Close()
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+        Return tabla.Rows(0)(0).ToString
+
+    End Function
+
+    'FUNCION PARA RECUPERAR LA FECHA ACTUAL DEL SISTEMA
+    Function RecuperarFechaActualSistema() As Date
+
+        miConexion.Open()
+        instruccionSQL = "select GETDATE()"
+        miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+        Dim adaptador As New SqlDataAdapter(miComandoSQL)
+        miConexion.Close()
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+        Return tabla.Rows(0)(0).ToString
+
+    End Function
+
+    'BOTON CONFIRMAR
+    Private Sub btnConfirmar_Click(sender As Object, e As EventArgs) Handles btnConfirmar.Click
+
+        If ValidarExisteAlumno(txtDocumento.Text) = True And ValidarEstadoAlumno(txtDocumento.Text) And
+           ValidarSeleccionLocal() = True And ValidarSeleccionModalidad() = True Then
+
+            Dim FechaActualSistema As Date = RecuperarFechaActualSistema()
+            Dim FechaAsistencia As Date = dtFechaAsistencia.Value
+            Dim LocalID As Integer = RecuperarLocalID()
+            Dim ModalidadID As Integer = RecuperarModalidadlID()
+            Dim HorarioID As Integer = RecuperarHorarioID()
+            Dim AlumnoID As Integer = RecuperarAlumnolID()
+            Dim Anulado As String = "N" 'Le puse como valor por defecto N ya que en el prototipo de
+            ' formulario no se ingresaba si estaba o no anulado
+            miConexion.Open()
+            instruccionSQL = "insert Reserva values ('" & FechaActualSistema & "','" & FechaAsistencia & "','" & LocalID & "','" & ModalidadID & "','" & HorarioID & "','" & AlumnoID & "','" & Anulado & "')"
+            miComandoSQL = New SqlCommand(instruccionSQL, miConexion)
+
+            miComandoSQL.ExecuteNonQuery()
+            miConexion.Close()
+
+            MsgBox("La reserva fue registrada correctamente")
+            txtDocumento.Clear()
+        Else
+
+            MsgBox("No se ha podido registrar la reserva")
+            txtDocumento.Clear() 'En este caso lo unico que se ingresa es el Nro de documento,
+            'al arrancar el formulario los combobox ya vienen con valores cargados, por eso fue el
+            'unico campo que limpie.
+
+        End If
+
+    End Sub
+
+End Class
+
